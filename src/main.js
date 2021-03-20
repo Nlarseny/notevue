e1763c4ace34b641db3b98ae3eb3b332aaf01ce0
Nlarseny <larsen.d.nathan@gmail.com>
2021-03-20 15:56:43 -0600

    works...

--- src/main.js
+++ src/main.js
@@ -5,20 +5,8 @@ import router from './router'
 Vue.config.productionTip = false
 
 let data = {
-  notes: [{
-    id: 1,
-    name: "Egg Salad",
-    content: "This is a test buddy boy",
-  }, {
-    id: 2,
-    name: "different",
-    content: "This boy",
-  }],
-  fav: [{
-    id: 2,
-    name: "different",
-    content: "This boy",
-  }]
+  notes: [],
+  fav: []
 }
 
 new Vue({
